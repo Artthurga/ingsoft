5bdd41ff7adb6e9b34fcef35f30dd6d8c32dc522
artthurga <artthurga@gmail.com>
2021-09-09 23:33:23 -0300

    Primer commit: el numero mayor

---
+++ mayor.php
@@ -0,0 +1,21 @@
+<?php
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$num3 = $_GET['num3'];
+$num4 = $_GET['num4'];
+$num5 = $_GET['num5'];
+$num6 = $_GET['num6'];
+
+while ($num1 && $num2 && $num3 && $num4 && $num5 && $num6 > 0) {
+    
+    $numeros = array($num1,$num2,$num3,$num4,$num5,$num6);
+
+    array_unique($numeros);
+
+Echo 'El mayor numero es: '. Max($numeros);
+
+break;
+
+};
+
+?>
